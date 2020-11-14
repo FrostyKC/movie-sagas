9776136dd0ef3884b69c2c04feccdacdf9c2f403
Joe Thompson <smnindian30@yahoo.com>
2020-11-13 18:05:37 -0600

    made getMovies saga, and errorMessage reducer

--- src/index.js
+++ src/index.js
@@ -9,48 +9,84 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
-
+  yield takeLatest('GET_MOVIES', getMovies);
 }
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
+function* getMovies(action) {
+  try {
+    yield put({ type: 'ERROR_RESET' });
+    const response = yield axios.get('/api/movie');
+    console.log(response.data);
+    // version of a dispatch = put
+    yield put({
+      type: 'SET_MOVIES',
+      payload: response.data,
+    });
+  } catch (err) {
+    console.log(err);
+    yield put({
+      type: 'ERROR_MSG',
+      payload: 'There was a problem loading movies. Please try again.',
+    });
+  }
+}
+
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_GENRES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const errorMessage = (state = null, action) => {
+  if (action.type === 'ERROR_MSG') {
+    return action.payload;
+  } else if (action.type === 'ERROR_RESET') {
+    return null;
+  }
+
+  return state;
+};
 
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        genres,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    genres,
+    errorMessage,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
-    document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
